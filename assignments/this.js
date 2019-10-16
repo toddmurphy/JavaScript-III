5609a4e6fbe1a0ff0b40d99b8fa88e9ab9fb0e28
toddmurphy <toddmurphy93@gmail.com>
2019-10-16 17:08:24 -0300

    updated questions for this assignment

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,11 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+The four principles for the "this" key word are:
+* 1.  Global or window binding - the global window lives in the brower as the main "window" of javascript. When you type in 'this' and run it, you can see a huge list of all the prototypes that live on the window object.
+* 2.  Implicit binding - when a function is called with the dot (.) to the left. For example person.dog would have this refer to the person object.
+* 3.  Explicit binding - is when you can use .call .bind. .apply to explicity or invoke or call a function or connect that function to an object and connect 'this' to the funciton.
+* 4.  New binding - we use constructor functions to create or return an object. We call or use the 'new' word to create an object such as a person, cat, dog, company,etc.
 *
 * write out a code example of each explanation above
 */
